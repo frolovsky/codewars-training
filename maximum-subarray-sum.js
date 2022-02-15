6618f7aff9c4bf9c6e4fcdbee55bcc9b4c25b65a
frolovsky <apf7517@yandex.ru>
2022-02-15 14:51:26 +0300

    add Kadane's algorithm solution

--- maximum-subarray-sum.js
+++ maximum-subarray-sum.js
@@ -25,10 +25,35 @@ const getMaxSubarray = (arr) => {
   return arr.slice(maxSubarrayStartIndex, maxSubarrayStartIndex + maxSubarrayLength);
 }
 
+const getMaxSubarrayDP = (arr) => {
+  const l = arr.length;
+  let maxSubarraySum = -Infinity;
+  let maxEndIndex = l - 1;
+  let maxStartIndex = 0;
+  let currentStartIndex = 0;
+  let currentSum = 0;
+
+  arr.forEach((currentNumber, currentIndex) => {
+    currentSum += currentNumber;
+
+    if (currentSum > maxSubarraySum) {
+      maxSubarraySum = currentSum;
+      maxEndIndex = currentIndex;
+      maxStartIndex = currentStartIndex;
+    }
+
+    if (currentSum < 0) {
+      currentSum = 0;
+      currentStartIndex = currentIndex + 1;
+    }
+  })
+  return arr.slice(maxStartIndex, maxEndIndex + 1);
+}
+
 const maxSequence = function(arr){
   if (!arr.some(n => n > 0)) return 0;
   if (arr.every(n => n >= 0)) sumArray(arr);
-  return sumArray(getMaxSubarray(arr));
+  return sumArray(getMaxSubarrayDP(arr));
 }
 
 console.log(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
